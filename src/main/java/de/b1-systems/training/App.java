a2034f34d84864dcb595137972e766ca8705fb09
Mattias Giese <giese@b1-systems.de>
2017-05-02 12:32:32 -0500

    basic stub initialized with mvn

---
+++ src/main/java/de/b1-systems/training/App.java
@@ -0,0 +1,13 @@
+package de.b1-systems.training;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
